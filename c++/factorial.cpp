225ae63d5855f2e778a1263026cadc2fce922488
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-03 02:39:27 +0530

    C++ program : dictionaries & maps

---
+++ c++/factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<stdio.h>
+
+using namespace std;
+long int factorial(int n);
+int main() {
+      int n;
+    cin>>n;
+    cout<<factorial(n);
+      return 0;
+}
+long int factorial(int n){
+    if(n>=1){
+ return n*factorial(n-1);
+       
+    }
+    else{
+        return 1;
+    }
+}
