8bfc579120573f576ac6fc8360ca433de8978cd8
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-02 16:07:56 +0530

    JS program : converting time into 24 hour format

---
+++ js-jquery/timein24hrformat.js
@@ -0,0 +1,13 @@
+<!-- Problem Statement : return time in 24 hour format according to given input-->
+
+
+        var submit = function () {
+            var input = '07:05:29AM';
+
+            if (input.substring(8, 10) == "PM") {
+                console.log((parseInt(input.substring(0,2))+12) + input.substring(2,8) );
+            }
+            else{
+                console.log(input.substring(0,8));
+            }
+        };
