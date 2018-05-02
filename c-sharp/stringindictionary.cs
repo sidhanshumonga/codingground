321af1607bae258d189a4fe3ff623b6a87373e80
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-02 16:10:41 +0530

    adding comments

--- c-sharp/stringindictionary.cs
+++ c-sharp/stringindictionary.cs
@@ -6,16 +6,18 @@ class Program
 {
     static void Main()
     {
+            // defining own dictionary
             Dictionary<string, int> dict = new Dictionary<string, int>();
             dict.Add("i",1);
             dict.Add("love",1);
             dict.Add("indian",1);
             dict.Add("food",1);
             dict.Add("spicy",1);
-            string input = Console.ReadLine();
-           //  Console.WriteLine(input.Length);
+            
+            string input = Console.ReadLine(); // input : ilovespicy
             bool flag = false;
             string check = "";
+        //looping through input string
             for(int i=0;i<input.Length;i++){
               check = check + input[i]; 
               Console.WriteLine(check);
