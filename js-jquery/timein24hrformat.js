2804896d28f1bff1d9706b6453620467689e5842
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-02 16:12:26 +0530

    adding comments

--- js-jquery/timein24hrformat.js
+++ js-jquery/timein24hrformat.js
@@ -1,8 +1,8 @@
-<!-- Problem Statement : return time in 24 hour format according to given input-->
+//Problem Statement : return time in 24 hour format according to given input
 
 
         var submit = function () {
-            var input = '07:05:29AM';
+            var input = '07:05:29AM'; // taking static input
 
             if (input.substring(8, 10) == "PM") {
                 console.log((parseInt(input.substring(0,2))+12) + input.substring(2,8) );
