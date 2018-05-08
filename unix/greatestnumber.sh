e237acdb3cf9f9fe6b41ffdf2944b989d817067a
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-08 11:26:05 +0530

    Unix program : find the greatest among three numbers

---
+++ unix/greatestnumber.sh
@@ -0,0 +1,19 @@
+echo "enter first number"
+read first
+echo "enter second number"
+read second
+echo "enter third number"
+read third
+if [ $first -gt $second ] ; then
+if [ $first -gt $third ] ; then
+echo -e " $first is greatest number "
+else
+echo -e " $third is greatest number "
+fi
+else
+if [ $second -gt $third ] ; then
+echo -e " $second is greatest number "
+else
+echo -e " $third is greatest number "
+fi
+fi
