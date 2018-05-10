7536031069bb64fc3b99781e67de0c3e80967a15
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-05-10 15:25:23 +0530

    C++ program : check whether given number is prime or not

---
+++ c++/primenumber.cpp
@@ -0,0 +1,31 @@
+// created by sidhanshu monga
+// 10 may, 2018
+
+//Problem statement :  check whether the given number is prime or not
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, i;
+  bool isPrime = true;
+
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  for(i = 2; i <= n / 2; ++i)
+  {
+      if(n % i == 0)
+      {
+          isPrime = false;
+          break;
+      }
+  }
+  if (isPrime)
+      cout << "This is a prime number";
+  else
+      cout << "This is not a prime number";
+
+  return 0;
+}
